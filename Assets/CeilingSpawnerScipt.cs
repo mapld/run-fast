8dc29b6079a08b8e52097c5c4b86ec704758f32c
Alex Rehnby <alrm014@gmail.com>
2014-11-08 17:37:20 -0800

    Added a spiky ceiling

---
+++ Assets/CeilingSpawnerScipt.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CeilingSpawnerScipt : MonoBehaviour {
+
+	public GameObject ceilingSpikes;
+
+	protected int counter;
+	public int baseTime;
+
+	// Use this for initialization
+	void Start () {
+		counter = (int) (baseTime / GlobalSpeed.globalSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter--;
+		if (counter <= 0) {
+			counter = (int) (baseTime / GlobalSpeed.globalSpeed);
+			if(Random.Range(0,100) < 50){
+				GameObject curSpikes = (GameObject)Instantiate (ceilingSpikes);
+				Destroy (curSpikes,25/(int)GlobalSpeed.globalSpeed);
+			}
+		}
+	}
+}
